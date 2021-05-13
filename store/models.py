499ed1130b4fe1cd9aab0cc05959998cf2ce12b2
returnpie <returnpie@icloud.com>
2021-05-14 00:47:30 +0900

    part 2

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -10,7 +11,7 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
 
     def get_absolute_url(self):
-        return reverse("store:category_list", args=[self.slug])
+        return reverse('store:category_list', args=[self.slug])
     
     def __str__(self):
         return self.name
@@ -33,6 +34,9 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = 'Products'
         ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
 
     def __str__(self):
         return self.title
--- store/tests.py
+++ store/tests.py
@@ -1,7 +1,14 @@
-from django.test import TestCase
+
+from unittest import skip
+
 from django.contrib.auth.models import User
+from django.http import HttpRequest
+from django.test import Client, RequestFactory, TestCase
+from django.urls import reverse
 
 from store.models import Category, Product
+from store.views import all_products
+
 
 class TestCateroriesModel(TestCase):
 
@@ -18,11 +25,53 @@ class TestCateroriesModel(TestCase):
 
 class TestProductsModel(TestCase):
     def setUp(self):
-        Category.objects.create(name='django', slug='django')
         User.objects.create(username='admin')
+        Category.objects.create(name='django', slug='django')
         self.data1 = Product.objects.create(category_id=1, title='django beginners', created_by_id=1, slug='django-beginners', price='20.00', image='django')
 
     def test_products_model_entry(self):
         data = self.data1
         self.assertTrue(isinstance(data, Product))
         self.assertEqual(str(data), 'django beginners')
+
+# skip example
+# @skip("reason")
+# class TestSkip(TestCase):
+#     def test_skip_example(self):
+#         pass
+
+class TestViewResponses(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.factory = RequestFactory()
+        User.objects.create(username='admin')
+        Category.objects.create(name='django', slug='django')
+        Product.objects.create(category_id=1, title='django beginners', created_by_id=1, slug='django-beginners', price='20.00', image='django')
+
+    def test_url_allowed_hosts(self):
+        response = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_url(self):
+        response = self.c.get(reverse('store:product_detail', args=['django-beginners']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_detail_url(self):
+        response = self.c.get(reverse('store:category_list', args=['django']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_html(self):
+        request = HttpRequest()
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_function(self):
+        request = self.factory.get('/item/django-begginners')
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
